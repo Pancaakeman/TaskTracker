d7a72870b319247189a5db7262aa5e03b6dab306
Chaitanya <haxionrising@gmail.com>
2026-01-19 11:17:28 +0000

    added querying of tasks

--- internal/queryTask.go
+++ internal/queryTask.go
@@ -15,3 +15,14 @@ func ListTasks() {
 		fmt.Printf("%v Task Name: %v \nTask Description: %v \nTask Urgency: %v \nTask Group: %v \n", i, task.Name, task.Desc, task.Urgency, task.Group)
 	}
 }
+func GetTaskWithName(name string) int {
+	i, exists := TaskIndex[name]
+	if !exists {
+		fmt.Printf("Given Task %v does not exist")
+	}
+	return i
+}
+func ListRangeOfTasks(lowerBound int, upperBound int) []Task {
+	return TaskList[lowerBound:upperBound]
+
+}
